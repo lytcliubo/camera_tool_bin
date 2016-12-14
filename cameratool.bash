73941ecd8accf347db74c586bda055000f72baf7
Leo.Liu <liubo0922@thundersoft.com>
2016-12-14 14:52:32 +0800

    Camera bash complate add restart_all func
    Change-Id: I026291f0568279db08782dd8fe7fc0892bc08c58

--- cameratool.bash
+++ cameratool.bash
@@ -1,4 +1,4 @@
-COMP="start_camera stop_camera restart_camera start_service stop_service restart_service"
+COMP="start_camera stop_camera restart_camera start_service stop_service restart_service restart_all"
 _cameratool() {
     cur=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=( $( compgen -W "$COMP" -- $cur) )
